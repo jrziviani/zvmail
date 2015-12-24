447398e90fec0651e736d9584559e7a95b540537
Jose Ricardo Ziviani <jose@ziviani.net>
2015-12-24 01:41:24 -0200

    Add copyright headers in the source files
    Signed-off-by: Jose Ricardo Ziviani <jose@ziviani.net>

--- build-aux/headers.py
+++ build-aux/headers.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Copyright (C) 2015 Jose Ricardo Ziviani - zmvar:3,17
+
 import sys
 import os
 import shutil
@@ -40,7 +56,7 @@ def apply_header(path, comment_symbol, line_to_start):
 
                 dest.write(line)
 
-    #shutil.move(path + '.tmp', path)
+    shutil.move(path + '.tmp', path)
 
 def main(argv):
     '''
